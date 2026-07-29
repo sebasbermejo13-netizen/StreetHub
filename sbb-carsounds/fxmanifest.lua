f436aa6027bfb9601880296d63ab9eef3ee10565
sebasbermejo13-netizen <sebasbermejo13@gmail.com>
2026-07-29 18:02:35 -0500

    Batch 1

---
+++ sbb-carsounds/fxmanifest.lua
@@ -0,0 +1,98 @@
+fx_version 'adamant'
+game 'gta5'
+
+author 'sbb'
+description 'sbb-carsounds - Sonidos de coches personalizados (15 sonidos)'
+version '1.0.0'
+
+files {
+	'audioconfig/*.dat151.rel',
+	'audioconfig/*.dat54.rel',
+	'audioconfig/*.dat10.rel',
+	'audioconfig/*.nametable',
+	'sfx/**/*.awc'
+}
+
+-- Sonido 1: lamveneng
+data_file 'AUDIO_SYNTHDATA' 'audioconfig/lamveneng_amp.dat'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/lamveneng_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/lamveneng_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_lamveneng'
+
+-- Sonido 2: npolchar
+data_file 'AUDIO_GAMEDATA' 'audioconfig/npolchar_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/npolchar_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_npolchar'
+
+-- Sonido 3: str12392m
+data_file 'AUDIO_GAMEDATA' 'audioconfig/str12392m_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/str12392m_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_str12392m'
+
+-- Sonido 4: gom12usav8a (Dominator GTS 2011 / gstdomgts1 + Bullet GTS OLD / gstbulgts1 -- comparten el mismo pack de audio)
+data_file 'AUDIO_SYNTHDATA' 'audioconfig/gom12usav8a_amp.dat'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/gom12usav8a_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/gom12usav8a_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_gom12usav8a'
+
+-- Sonido 5: gom10euri6b (Sentinel XS4 M3E46 / gstsentgt1)
+data_file 'AUDIO_SYNTHDATA' 'audioconfig/gom10euri6b_amp.dat'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/gom10euri6b_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/gom10euri6b_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_gom10euri6b'
+
+-- Sonido 6: lfasound (Emperor LFC / gstlfc1)
+data_file 'AUDIO_GAMEDATA' 'audioconfig/lfasound_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/lfasound_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_lfasound'
+
+-- Sonido 7: ml720v8eng (Progen 920S / gst920s1)
+data_file 'AUDIO_SYNTHDATA' 'audioconfig/ml720v8eng_amp.dat'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/ml720v8eng_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/ml720v8eng_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_ml720v8eng'
+
+-- Sonido 8: gom07sportjpi6 (Maibatsu Sunrise RS / gstsunrisers1 + SupraMK4 / gstjrz1 -- comparten el mismo pack de audio)
+data_file 'AUDIO_SYNTHDATA' 'audioconfig/gom07sportjpi6_amp.dat'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/gom07sportjpi6_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/gom07sportjpi6_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_gom07sportjpi6'
+
+-- Sonido 9: elegyx (Nissan R35 Nismo - Elegy / elegyrh35)
+data_file 'AUDIO_GAMEDATA' 'audioconfig/elegyx_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/elegyx_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_elegyx'
+
+-- Sonido 10: str10b302 (Ford Mustang Boss 302 V12 swap)
+data_file 'AUDIO_GAMEDATA' 'audioconfig/str10b302_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/str10b302_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_str10b302'
+
+-- Sonido 11: lg58mwm3gtrdemo (BMW M3 GTR P60B40) -- este era el hash que usaba enviz antes de reasignarlo a "cypher"
+data_file 'AUDIO_SYNTHDATA' 'audioconfig/lg58mwm3gtrdemo_amp.dat'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/lg58mwm3gtrdemo_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/lg58mwm3gtrdemo_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_lg58mwm3gtrdemo'
+
+-- Sonido 12: lg101timcamz28 (Chevrolet Camaro Z28 LS7)
+data_file 'AUDIO_SYNTHDATA' 'audioconfig/lg101timcamz28_amp.dat'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/lg101timcamz28_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/lg101timcamz28_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_lg101timcamz28'
+
+-- Sonido 13: ta488f154 (Ferrari 488 / F8 / SF90 -- motor F154)
+data_file 'AUDIO_GAMEDATA' 'audioconfig/ta488f154_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/ta488f154_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_ta488f154'
+
+-- Sonido 14: lg123murcisp (Lamborghini Murcielago V12 escape recto)
+data_file 'AUDIO_SYNTHDATA' 'audioconfig/lg123murcisp_amp.dat'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/lg123murcisp_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/lg123murcisp_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_lg123murcisp'
+
+-- Sonido 15: f136 (Ferrari 458 -- motor F136)
+data_file 'AUDIO_SYNTHDATA' 'audioconfig/f136_amp.dat'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/f136_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/f136_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_f136'
